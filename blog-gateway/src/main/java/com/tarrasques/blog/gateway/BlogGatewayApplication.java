0a8ac8e30f85b140f4100b01aa80312fbb44537c
tiamou <1417223554@qq.com>
2023-06-01 15:31:21 +0800

    整合nacos

--- blog-gateway/src/main/java/com/tarrasques/blog/gateway/BlogGatewayApplication.java
+++ blog-gateway/src/main/java/com/tarrasques/blog/gateway/BlogGatewayApplication.java
@@ -3,9 +3,11 @@ package com.tarrasques.blog.gateway;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.tarrasques.blog.gateway.mapper")
+@EnableDiscoveryClient
 public class BlogGatewayApplication {
 
     public static void main(String[] args) {
